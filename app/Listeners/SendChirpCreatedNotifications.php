f0d792ad99ce86c25802ef87d2902c952148ba6d
Towns87 <townsendmatt1987@gmail.com>
2025-01-07 10:46:39 -0500

    git

---
+++ app/Listeners/SendChirpCreatedNotifications.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ChirpCreated;
+use App\Models\User;
+use App\Notifications\NewChirp;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendChirpCreatedNotifications implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(ChirpCreated $event): void
+    {
+        foreach (user::whereNot('id', $event->chirp->user_id)->cursor() as $user) {
+            $user->notifu(new NewChirp($event->chirp));
+        }
+    }
+}
---
+++ app/Models/Chirp.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+
+use App\Events\ChirpCreated;
+use Illumintate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
+class Chirp extends Model
+{
+    protected $fillable = [
+        'message'
+    ];
+    
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+
+
+    public function user (): Belongsto
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
